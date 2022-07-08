ac0dc2306b8637340ef9fafa8100ebba4b6fe014
dsoto1-rbi <dsoto1@rbi.com>
2022-07-08 12:25:15 -0500

    [feature] - set toolbar in main activity

--- app/src/main/java/com/danielsoto/toolbarexample/MainActivity.java
+++ app/src/main/java/com/danielsoto/toolbarexample/MainActivity.java
@@ -1,8 +1,14 @@
 package com.danielsoto.toolbarexample;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.app.ActionBar;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_item, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.share_icon:
+                Toast.makeText(this, "Share", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.folder_icon:
+                Toast.makeText(this, "Folder", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.notification_icon:
+                Toast.makeText(this, "Notification", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menu_icon:
+                Toast.makeText(this, "Menu", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return true;
     }
 }
